ce93c14e19132bbe697a56284ddb402f56dbbd0f
Denis <dev@webernetic.by>
2022-09-18 19:54:13 +0300

    add validation

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.validation.DateValidation;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -18,16 +21,23 @@ public class FilmController {
     }
 
     @PostMapping(value = "/film")
-    public Film create(@RequestBody Film film) {
+    public Film create(@Valid @RequestBody Film film) {
+        if ( !(new DateValidation(film.getReleaseDate()).checkDate()) ) {
+            throw new ValidationException("Release date must be greater than 1895-12-28");
+        }
         films.add(film);
         return film;
     }
 
     @PutMapping(value = "/films/{filmId}")
-    public Film update(@RequestBody Film film, @PathVariable Long filmId) {
+    public Film update(@Valid @RequestBody Film film, @PathVariable Long filmId) {
         final List<Film> filmList = films.stream().filter(f -> Objects.equals(f.getId(), filmId)).toList();
         if (filmList.size() == 0) {
             throw new NotFoundException();
+        }
+
+        if ( !(new DateValidation(film.getReleaseDate()).checkDate()) ) {
+            throw new ValidationException("Release date must be greater than 1895-12-28");
         }
 
         final Film currentFilm = filmList.get(0);
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -4,10 +4,10 @@ import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Stream;
 
 @RestController
 public class UserController {
@@ -19,23 +19,31 @@ public class UserController {
     }
 
     @PostMapping(value = "/user")
-    public User create(@RequestBody User user) {
-        users.add(user);
+    public User create(@Valid @RequestBody User user) {
+        users.add(getNameIfEmpty(user));
         return user;
     }
 
     @PutMapping(value = "/users/{userId}")
-    public User update(@RequestBody User user, @PathVariable Long userId) {
+    public User update(@Valid @RequestBody User user, @PathVariable Long userId) {
+        User checkedUser = getNameIfEmpty(user);
         final List<User> userById = users.stream().filter(u -> Objects.equals(u.getId(), userId)).toList();
         if (userById.size() == 0) {
             throw new NotFoundException();
         }
 
         final User currentUser = userById.get(0);
-        currentUser.setEmail(user.getEmail());
-        currentUser.setLogin(user.getLogin());
-        currentUser.setName(user.getName());
-        currentUser.setBirthday(user.getBirthday());
+        currentUser.setEmail(checkedUser.getEmail());
+        currentUser.setLogin(checkedUser.getLogin());
+        currentUser.setName(checkedUser.getName());
+        currentUser.setBirthday(checkedUser.getBirthday());
+        return checkedUser;
+    }
+
+    private User getNameIfEmpty(User user) {
+        if (user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
         return user;
     }
 }
