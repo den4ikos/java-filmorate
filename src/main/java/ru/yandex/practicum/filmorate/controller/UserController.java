bdb2efcaf38a5ce6e2801d11c52c44abcb50bb46
Denis <dev@webernetic.by>
2022-10-02 14:29:17 +0300

    move user logic from controller to service

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,61 +2,40 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
 public class UserController {
-    private List<User> users = new ArrayList<>();
-    private Long id = 0L;
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping(value = "/users")
     public List<User> findAll(HttpServletRequest request) {
         log.info("Endpoint request received: '{} {}', Query Parameter String: '{}'",
                 request.getMethod(), request.getRequestURI(), request.getQueryString());
-        return users;
+        return userService.findAll();
     }
 
     @PostMapping(value = "/users")
     public User create(@Valid @RequestBody User user) {
-        id++;
-        user.setId(id);
-        users.add(getNameIfEmpty(user));
         log.info("Endpoint request received: 'POST/user' {}'", user.toString());
-        return user;
+        return userService.create(user);
     }
 
     @PutMapping(value = "/users")
     public User update(@Valid @RequestBody User user) {
-        User checkedUser = getNameIfEmpty(user);
-        final List<User> userById = users.stream().filter(u -> Objects.equals(u.getId(), user.getId())).collect(Collectors.toList());
-
-        if (userById.size() == 0) {
-            log.error("There is no any user!");
-            throw new NotFoundException();
-        }
-
-        final User currentUser = userById.get(0);
-        currentUser.setEmail(checkedUser.getEmail());
-        currentUser.setLogin(checkedUser.getLogin());
-        currentUser.setName(checkedUser.getName());
-        currentUser.setBirthday(checkedUser.getBirthday());
         log.info("Endpoint request received: 'POST/films/{} {}'", user.getId(), user.toString());
-        return checkedUser;
+        return userService.update(user);
     }
 
-    private User getNameIfEmpty(User user) {
-        if (user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
-            user.setName(user.getLogin());
-        }
-        return user;
-    }
+
 }
--- src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/UserService.java
@@ -1,7 +1,56 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import ru.yandex.practicum.filmorate.exception.NotFoundException;
+import ru.yandex.practicum.filmorate.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Service
 public class UserService {
+    private Long id = 0L;
+    private List<User> users = new ArrayList<>();
+
+    public void incrementId() {
+        id++;
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+
+    public User create(User user) {
+        incrementId();
+        user.setId(id);
+        users.add(getNameIfEmpty(user));
+        return user;
+    }
+
+    public User update(User user) {
+        User validUser = getNameIfEmpty(user);
+        User currentUser = users.stream()
+                .filter(u -> u.getId().equals(user.getId()))
+                .findFirst()
+                .orElseThrow(() -> {
+                    log.error("There is no any user!");
+                    return new NotFoundException();
+                });
+        currentUser.setEmail(validUser.getEmail());
+        currentUser.setLogin(validUser.getLogin());
+        currentUser.setName(validUser.getName());
+        currentUser.setBirthday(validUser.getBirthday());
+
+        return currentUser;
+    }
+
+    private User getNameIfEmpty(User user) {
+        if (user.getName() == null || user.getName().isEmpty() || user.getName().isBlank()) {
+            user.setName(user.getLogin());
+        }
+        return user;
+    }
 }
